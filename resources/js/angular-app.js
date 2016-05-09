dcfe0b59e291bfce368ab6c5d5d665cae0d03b1f
Dan L <dlogajan@gmail.com>
2016-05-09 12:34:08 -0400

    initial file upload

---
+++ resources/js/angular-app.js
@@ -0,0 +1,58 @@
+/***************************
+  PRE-PLAN SKETCH
+  ---------------
+  
+  ON PAGE LOAD -
+    ^REQUEST JSON AND SAVE TO VARIABLE
+    CREATE LIST OF GAMES FOR DROPDOWN MENU
+    INIT NEW GAME FROM NEWEST DATE
+    
+  INIT NEW GAME -
+    RESET ANY NECESSARY DATA (SCOREBOARD,)
+    FILL IN MOVIE CARDS
+    
+  SUBMIT EACH GUESS -
+    HIDE SLIDER AND BUTTON
+  ++IT MIGHT BE EASIER AND MORE ATTRACTIVE TO USE A HEIGHT ADJUSTMENT FOR A COLLAPSING EFFECT
+  ++NOPE. NG-HIDE ANIMATION
+    SHOW OTHER PLAYER GUESSES
+    SHOW TOMATO SCORE
+   ?SHOW VARIANCE IN SCORE
+    UPDATE SCOREBOARD
+    
+    
+  JSON MODEL
+  ----------
+DATA.
+  {DATABASE:
+    {
+      CATEGORY:
+      DATE:
+      MOVIES: [
+        {
+          TITLE:
+          URL:
+          SCORE:
+          GUESSES: [
+            ACE:
+            BRYAN:
+            GINA:
+          ]
+        }
+      ]
+    }
+  }
+***************************/
+
+var app = angular.module('gameApp', []);
+
+app.controller('gameCtrl', function($scope, $http) {
+  $http.get('/json/strung.json')
+  .then(function(response) {
+    init(response.data);
+  });
+});
+
+function init(obj) {
+  
+}
