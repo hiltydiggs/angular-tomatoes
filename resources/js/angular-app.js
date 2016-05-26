b4d2f6762d422be525952fcdd6d2fb606fa861a6
Dan Logajan <dlogajan@gmail.com>
2016-05-26 00:18:24 -0500

    Created score keeping service and ScoreController

--- resources/js/angular-app.js
+++ resources/js/angular-app.js
@@ -45,24 +45,57 @@ DATA.
 ***************************/
 
 angular.module('gameApp', [])
-  .controller('DatabaseController', ['$scope', 'databaseFactory', DatabaseController])
-  .controller('SubmitController', ['$scope', SubmitController])
+  .controller('DatabaseController', ['$scope', 'databaseFactory', 'scoreService', DatabaseController])
+  .controller('SubmitController', ['$scope', 'scoreService', SubmitController])
+  .controller('ScoreController', ['$scope', 'scoreService', ScoreController])
+  .service('scoreService', [scoreService])
   .factory('databaseFactory', ['$http', databaseFactory]);
 
-function DatabaseController($scope, databaseFactory) {
+function DatabaseController($scope, databaseFactory, scoreService) {
   databaseFactory.getDatabase()
     .then(function(response) {
+      var scorePath = response.data.database[0].movies[0].scores;
       $scope.movie = response.data;
+      angular.forEach(scorePath, function(value, key){
+        scoreService.addNames(key);
+      });
     });
 }
 
-function SubmitController($scope) {
+function SubmitController($scope, scoreService) {
   $scope.userGuess = 50;
   $scope.isSubmitted = false;
   $scope.submit = function () {
-    console.log($scope.userGuess);
+    scoreService.addScore('You', $scope.userGuess, $scope.movie.tomatoScore);
+    angular.forEach($scope.movie.scores, function(value, key){
+      scoreService.addScore(key, value, $scope.movie.tomatoScore);
+    });
+    console.log(scoreService.returnNames());
     $scope.isSubmitted = true;
-    console.log(Math.abs($scope.userGuess - $scope.movie.tomatoScore));
+  }
+}
+
+function ScoreController($scope, scoreService) {
+  $scope.scoreboard = scoreService.returnNames();
+}
+
+function scoreService() {
+  var scores = {'You': 0};
+
+  return {
+    addNames: function(name) {
+      scores[name] = 0;
+    },
+    returnNames: function() {
+      return scores;
+    },
+    addScore: function(player, guess, actual) {
+      if (parseInt(guess) === parseInt(actual)) {
+        scores[player] -= 5;
+      } else {
+        scores[player] += Math.abs(guess - actual);
+      }
+    }
   }
 }
 
